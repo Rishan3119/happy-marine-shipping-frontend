6880d1e983f394c0beaeb687b873ed7cea106459
Rishan3119 <Rishan3119@gmail.com>
2025-01-28 08:45:55 +0400

    Logistics

--- src/function/config.js
+++ src/function/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    base_url: "https://happy-marine-shipping-backend.onrender.com"
+    base_url: "https://happy-marine-shipping-new-backen.onrender.com"
     // base_url: "http://127.0.0.1:8000"
    
 
