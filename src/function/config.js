209552a710c8aee4549e56b0be23bb66128213bb
Rishan3119 <Rishan3119@gmail.com>
2025-02-19 15:26:55 +0400

    registrations completed

--- src/function/config.js
+++ src/function/config.js
@@ -1,7 +1,7 @@
 
 const config = {
-    // base_url: "https://happy-marine-shipping-new-backen.onrender.com"
-    base_url: "http://127.0.0.1:8000"
+    base_url: "https://happy-marine-shipping-new-backen.onrender.com"
+    // base_url: "http://127.0.0.1:8000"
     // base_url: "https://backend.happymarine.ae"
    
 
